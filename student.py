a7995c36cfec903e3542334d350a5b9a398e08dd
watkissp <watkissp@gilmour.org>
2018-10-17 14:17:10 -0400

    Add open house movement

--- student.py
+++ student.py
@@ -46,6 +46,7 @@ class Piggy(pigo.Pigo):
                 "o": ("Obstacle count", self.obstacle_count),
                 "c": ("Calibrate", self.calibrate),
                 "s": ("Check status", self.status),
+                "h": ("Open House", self.open_house),
                 "q": ("Quit", quit_now)
                 }
         # loop and print the menu...
@@ -55,6 +56,16 @@ class Piggy(pigo.Pigo):
         ans = raw_input("Your selection: ")
         # activate the item selected
         menu.get(ans, [None, error])[1]()
+
+    def open_house(self):
+        """reacts to dist measurement in a cute way"""
+        while True:
+            if self.dist() < 20:
+                self.encB(3)
+                self.encF(3)
+                self.servo(self.MIDPOINT - 30)
+                self.servo(self.MIDPOINT + 30)
+            time.sleep(.1)
 
     # YOU DECIDE: How does your GoPiggy dance?
     def dance(self):
