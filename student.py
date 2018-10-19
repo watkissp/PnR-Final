533b02399acaffada75f8d2d4dcf4a7dae7e431d
watkissp <watkissp@gilmour.org>
2018-10-19 14:26:33 -0400

    changes obstacle count

--- student.py
+++ student.py
@@ -162,7 +162,7 @@ class Piggy(pigo.Pigo):
         found_something = False
         counter = 0
         for ang, distance in enumerate(self.scan):
-            if distance and distance < 200 and not found_something:
+            if distance and distance < 20 and not found_something:
                 found_something = True
                 counter += 1
                 print("Object # %d found, I think" % counter)
