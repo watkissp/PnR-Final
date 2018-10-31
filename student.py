aa54243758a5b65ae8598c49085825500c3e490f
watkissp <watkissp@gilmour.org>
2018-10-31 14:43:54 -0400

    changes is_clear to scan more area

--- student.py
+++ student.py
@@ -240,6 +240,26 @@ class Piggy(pigo.Pigo):
                 self.fwd()
 ####################################################
 ############### STATIC FUNCTIONS
+    def is_clear(self):
+        """does a 3-point scan around the midpoint, returns false if a test fails"""
+        print("Running the is_clear method.")
+        for x in range((self.MIDPOINT - 35), (self.MIDPOINT + 35), 5):
+            self.servo(x)
+            scan1 = self.dist()
+            # double check the distance
+            scan2 = self.dist()
+            # if I found a different distance the second time....
+            if abs(scan1 - scan2) > 2:
+                scan3 = self.dist()
+                # take another scan and average the three together
+                scan1 = (scan1 + scan2 + scan3) / 3
+            self.scan[x] = scan1
+            print("Degree: " + str(x) + ", distance: " + str(scan1))
+            if scan1 < self.SAFE_STOP_DIST:
+                print("Doesn't look clear to me")
+                return False
+        return True
+
 
 def error():
     """records general, less specific error"""
