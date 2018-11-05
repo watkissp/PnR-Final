e18d933bc82894bd2b231c4623d19ca2137c6b89
watkissp <watkissp@gilmour.org>
2018-11-05 14:59:12 -0500

    changes motor speeds

--- student.py
+++ student.py
@@ -23,9 +23,9 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 35
         self.HARD_STOP_DIST = 20
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 135
+        self.LEFT_SPEED = 95
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.RIGHT_SPEED = 145
+        self.RIGHT_SPEED = 135
         # This one isn't capitalized because it changes during runtime, the others don't
         self.turn_track = 0
         # Our scan list! The index will be the degree and it will store distance
