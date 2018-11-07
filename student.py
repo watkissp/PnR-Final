93b64a002693aec47d788bcbfedc614355c9bdb4
watkissp <watkissp@gilmour.org>
2018-11-07 15:15:48 -0500

    motor speed change

--- student.py
+++ student.py
@@ -23,7 +23,7 @@ class Piggy(pigo.Pigo):
         self.SAFE_STOP_DIST = 35
         self.HARD_STOP_DIST = 20
         # YOU DECIDE: What left motor power helps straighten your fwd()?
-        self.LEFT_SPEED = 190
+        self.LEFT_SPEED = 135
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.RIGHT_SPEED = 190
         # This one isn't capitalized because it changes during runtime, the others don't
